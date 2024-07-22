5c32a08cdd4ec3764681ae2901ff480654a64139
Oğuzhan <oguzhancevik.developer@gmail.com>
2024-07-22 15:54:42 +0300

    shared class lib added

---
+++ src/Shared/Microservices.Shared/Events/StockNotReservedEvent.cs
@@ -0,0 +1,10 @@
+﻿using Microservices.Shared.Events.Common;
+
+namespace Microservices.Shared.Events;
+
+public class StockNotReservedEvent : IEvent
+{
+    public Guid BuyerId { get; set; }
+    public Guid OrderId { get; set; }
+    public string Message { get; set; } = default!;
+}
